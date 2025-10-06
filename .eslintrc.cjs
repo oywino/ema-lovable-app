3c7bfecb34a840c29515e456a78f6149788b787c
oywino <oyvindo@compuserve.com>
2025-10-06 12:48:39 +0200

    Create .eslintrc.cjs
    Code quality (ESLint + Prettier)

---
+++ .eslintrc.cjs
@@ -0,0 +1,20 @@
+/* eslint config: minimal, React + TS + Prettier */
+module.exports = {
+  root: true,
+  env: { browser: true, es2021: true, node: true },
+  parser: "@typescript-eslint/parser",
+  parserOptions: { ecmaVersion: "latest", sourceType: "module" },
+  settings: { react: { version: "detect" } },
+  plugins: ["@typescript-eslint", "react", "react-hooks", "jsx-a11y"],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:jsx-a11y/recommended",
+    "prettier"
+  ],
+  rules: {
+    "react/react-in-jsx-scope": "off" // not needed with Vite/React
+  }
+};
